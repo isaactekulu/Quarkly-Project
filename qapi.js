262e8037727d345802b3fbf3e12ccba941e8bd97
isaactekulu <isaactekulu@gmail.com>
2021-07-06 10:43:04 +1100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60e39819a4e264001e65387c",
+				"60e39819a4e264001e65387e"
+			]
+		},
+		"60e39819a4e264001e65387c": {
+			"id": "60e39819a4e264001e65387c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60e39819a4e264001e65387e": {
+			"id": "60e39819a4e264001e65387e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
